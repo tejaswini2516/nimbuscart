fab01531fcad758fca26cd8706778e1641b1f82a
tejaswini2516 <140441508+tejaswini2516@users.noreply.github.com>
2026-08-27 18:42:10 +0530

    Add frontend routing and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Cart from "./pages/Cart/Cart";
+import Login from "./pages/Login/Login";
+
 function App() {
   return (
-    <>
-      <h1>NimbusCart</h1>
-      <p>Cloud Native E-Commerce Platform</p>
-    </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
